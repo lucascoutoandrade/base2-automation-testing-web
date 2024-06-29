7ca977caf9c72a2e6fd915bf81a82ba235e04d3d
Lucas <lucascouto.ads@gmail.com>
2024-06-29 10:22:33 -0300

    removed all commented lines

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,19 +1,15 @@
 package tests;
 
 import java.util.Properties;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
 import core.DriverFactory;
 import utils.ConfigFileReader;
 import utils.Enums.Navegadores;
 
 public class BaseTest {
 
-//	public final static String url = "https://mantis-prova.base2.com.br";
 	private static Properties prop = ConfigFileReader.getProp();
 
 	@BeforeEach
